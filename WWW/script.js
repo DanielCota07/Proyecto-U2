403f96a64c38cc402b6037bd7444c027150372e2
Daniel Cota <danielalbertocotaochoa6@gmail.com>
2021-05-19 11:19:45 -0600

    Se terminó la pagina principal y se agregaron dos imagenes.

--- WWW/script.js
+++ WWW/script.js
@@ -8,10 +8,12 @@ fetch(url)
             let li = document.createElement("li");
             li.innerHTML = `
                 <a href="detalle.html">
-                    <img class="carro__imagen" src=${car.url} alt=${car.alter}>
-                    <div class="carro__inf">
-                        <p class="producto__nombre">${car.name}</p>
-                        <p class="producto__precio">$</p>
+                    <div class="carros">
+                        <img class="carro__imagen" src=${car.url} alt=${car.alter}>
+                        <div class="carro__inf">
+                            <p class="producto__nombre">Nombre: ${car.name}</p>
+                            <p class="producto__precio">Precio: ${car.precio}</p>
+                        </div>
                     </div>
                 </a>`
             list.appendChild(li);
